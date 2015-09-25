54d4dd91e0062fe31a8bff0e8802b85874a1c3be
Audric PANDELE <audric.pandele@ynov.com>
2015-09-25 10:24:53 +0200

    Add devise

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,7 +10,7 @@ angular.module('WishList').controller('MainCtrl', function($scope, $http) {
     });
   }
 
-	$scope.addToWishlist = function(price) {
+	$scope.addToWishlist = function(price, devise) {
     $scope.prograssing = true;
     $scope.added = false;
     getCookies("http://localhost:9000", "list_id", function(id) {
@@ -22,9 +22,7 @@ angular.module('WishList').controller('MainCtrl', function($scope, $http) {
 
             $http.post("http://0.0.0.0:9292/wishlistslinks?link="+url+"&wishlist_id="+list_id)
             .success(function(data){
-              setPrice(data.id, price)
-              $scope.prograssing = false;
-              $scope.added = true;
+              setPrice(data.id, price, devise)
             });
         });
       }else {
@@ -35,8 +33,9 @@ angular.module('WishList').controller('MainCtrl', function($scope, $http) {
 
 	};
 
-  function setPrice(id, price){
-    $http.put("http://0.0.0.0:9292/wishlistslinks/"+id+"?price="+price)
+  function setPrice(id, price, devise){
+    console.log(devise);
+    $http.put("http://0.0.0.0:9292/wishlistslinks/"+id+"?price="+price+"&devise="+devise)
     .success(function(data){
       $scope.prograssing = false;
       $scope.added = true;
